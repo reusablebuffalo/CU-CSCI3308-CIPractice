925331768ca0f9c06d43e92359e7bc986f7a9c87
Ian Smith <papachingching@gmail.com>
2018-04-18 12:01:05 -0600

    added function and function tests
    implemented triangle area function and 3 unit tests for it

--- geometry_test.c
+++ geometry_test.c
@@ -145,6 +145,37 @@ START_TEST(test_2d_midpoint)
 }
 END_TEST
 
+/* coord_2d_area_triangle Test */
+START_TEST(test_2d_area_triangle)
+{
+    coord_2d_t a;
+    coord_2d_t b;
+    coord_2d_t c;
+
+    /* all at same point, area should be 0 */
+    a.x = b.x = c.x = 0;
+    a.y = b.y = c.y = 0;
+    double res1 = coord_2d_area_triangle(&a,&b,&c);
+    ck_assert(res1 == 0);
+
+    /* when colinear, area should be 0 */
+    a.x = 0;
+    b.x = 5;
+    c.x = 10;
+    a.y = b.y = c.y = 0; // all colinear
+    double res2 = coord_2d_area_triangle(&a, &b, &c);
+    ck_assert(res2 == 0);
+
+    /* right triangle with area of 100 */
+    a.x = b.x = a.y = c.y = 5;
+    c.x = 25;
+    b.y = 15;
+    double res3 = coord_2d_area_triangle(&a, &b, &c);
+    ck_assert(res3 == 100);
+
+}
+END_TEST
+
 /* coord_2d Test Suite */
 Suite* coord_2d_suite(void)
 {
@@ -162,10 +193,14 @@ Suite* coord_2d_suite(void)
     TCase* tc_2d_midpoint = tcase_create("coord_2d_midpoint");
     tcase_add_test(tc_2d_midpoint, test_2d_midpoint);
 
+    TCase* tc_2d_area_triangle = tcase_create("coord_2d_area_triangle");
+    tcase_add_test(tc_2d_area_triangle, test_2d_area_triangle);
+
     /* Add Cases to Suite */
     suite_add_tcase(s, tc_2d_eq);
     suite_add_tcase(s, tc_2d_dist);
     suite_add_tcase(s, tc_2d_midpoint);
+    suite_add_tcase(s, tc_2d_area_triangle);
 
     /* Return Suite */
     return s;
